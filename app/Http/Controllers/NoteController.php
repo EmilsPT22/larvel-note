d7e42ee652eb9bebe9d00254dcac2d1bd4747f2f
Your Name <you@example.com>
2024-10-23 12:42:51 +0300

    display all posts

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Note;
 
 class NoteController extends Controller
 {
-    //
+    public function index(){
+        $posts = Note::all();
+        return viwe('note.index', ['allNotes' => $notes]);
+    }
 }
